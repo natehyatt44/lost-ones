7560f528afc6f5e6e4073038df4fd962a170cce8
natehyatt44 <nate.hyatt@gmail.com>
2023-01-31 16:36:22 -0700

    Site Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,106 +6,86 @@ function Home() {
    <> 
    <div className="container-fluid bg-nav">
    <nav className="navbar navbar-expand-lg navbar-light ">
-      <a className="navbar-brand " href="/"><img className="logo_s" src="assets/images/biLogo.png"/></a>
+      <a className="navbar-brand " href="/"><img className="logo_s" src="assets/css/logo.png"/></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarText">
          <ul className="navbar-nav m-auto">
-            <li className="nav-item"> <a className="navbar-brand hd_m" href="/"><img className="logo_s logo_m" src="assets/images/logo.png"/></a>
+            <li className="nav-item"> <a className="navbar-brand hd_m" href="/"><img className="logo_s logo_m" src="assets/css/logo.png"/></a>
             </li>
             <li className="nav-item">
-               <a className="nav-link" href="#tokenmics">Roadmap</a>
+               <a className="nav-link" href="#about">About</a>
             </li>
             <li className="nav-item">
-               <a className="nav-link" href="#tokenmics">Holder Benefits</a>
+               <a className="nav-link" href="#gallery">Gallery</a>
             </li>
             <li className="nav-item">
-               <a className="nav-link" href="#buy">LitePaper</a>
+               <a className="nav-link" href="#team">Team</a>
             </li>
             <li className="nav-item">
-               <a className="nav-link" href="https://docs.nebulanodes.finance">Gallery</a>
+               <a className="nav-link" href="https://docs.nebulanodes.finance">LitePaper</a>
             </li>
             <li className="nav-item">
-               <a className="nav-link" href="/Dapp">Founders</a>
+               <a className="nav-link" href="#roadmap">RoadMap</a>
             </li>
             <li className="nav-item">
-               <a className="nav-link" href="/Dapp">FAQ</a>
+               <a className="nav-link" href="#faq">FAQ</a>
             </li>
          </ul>
          <span className="navbar-text" >
-         <a href="https://twitter.com/NebulaNodes" target={{target:"_blank"}}>
-         <img className="seicon" src="assets/images/icon/icon_twitter.svg"/>
+         <a href="https://twitter.com/barbarianinc" target={{target:"_blank"}}>
+         <img className="seicon" src="assets/css/icon_twitter.svg"/>
          </a>
-         <a href="https://discord.com/invite/dk8pCznAJP" target={{target:"_blank"}}>
-         <img className="seicon" src="assets/images/icon/icon_discord.svg"/>
+         <a href="https://twitter.com/barbarianinc" target={{target:"_blank"}}>
+         <img className="seicon" src="assets/css/icon_discord.svg"/>
          </a>
          </span>
       </div>
    </nav>
 </div>
-<section id="About_nebula " className="first_sec ">
-   <div id="home" className="container  nebula_sec">
+<section id="banner " className="banner_sec "></section>
+<section id="about" className="info_sec ">
+   <div className="container  barb_sec">
       <div className="row">
          <div className="col-sm-12 col-md-12 col-lg-12">
-            <h1 className="h1_head_sml">Barbarian Inc </h1>
-         </div>
-      </div>
-   </div>
-</section>
-<span className="circledot"></span>
-<section id="overseer " className="second_sec ">
-   <div className="container  nebula_sec">
-      <div className="row">
-         <div className="col-sm-12 col-md-12 col-lg-12">
-            <h1 className="h1_heading set_font">The Overseer </h1>
+            <h1 className="h1_heading set_font">Barbarian Inc </h1>
             <p className="para_p  ">
-               Our protocol is watched meticulously by our Overseer System day <br/>and night. Keeping your mind at ease as it buys on the
-               lower peg  <br/> and sells on the upper peg,  ensuring the price doesn't skyrocket or <br/>collapse! Meanwhile filling the
-               Liquidity pool and itself to ensure a <br/>smooth price bound token.
+               We are Bussin fr fr
             </p>
-            <div className="btn_soc">
-               <a  className="btn_ion" href="https://docs.nebulanodes.finance" target={{target:"_blank"}}>
-               link to docs </a>
-            </div>
          </div>
       </div>
    </div>
 </section>
-<span className="dimonds"></span>
-<section id="About_nebula " className="third_sec token_nomics ">
-   <div id="tokenmics" className="container  nebula_sec">
+<section id="gallery " className="info_sec ">
+   <div id="gallery" className="container  barb_sec">
       <div className="row">
          <div className="col-sm-12 col-md-12 col-lg-10">
-            <h1 className="h1_heading set_font"> Tokenomics</h1>
-            <p className="para_p  ">1.) Total Supply : 1,200,000 NeFi Tokens. <br/>
-               2.) Payout : 0.1 of a token per day. <br/>
-               3.) Price per node : 10 NeFi tokens 150,000.<br/>
-            </p>
+            <h1 className="h1_heading set_font"> Gallery</h1>
+            <div className="col-sm-12 col-md-12 col-lg-10"><img className="gallery_img" src="assets/css/8.png"/></div>
          </div>
+         
       </div>
    </div>
 </section>
-<section id="About_nebula " className="third_sec hwtby ">
-   <div id="buy" className="container  nebula_sec">
+<section id="team " className="info_sec ">
+   <div id="team" className="container  barb_sec">
       <div className="row">
          <div className="col-sm-12 col-md-12 col-lg-10">
-            <h1 className="h1_heading set_font"> How To buy</h1>
-            <p className="para_p  ">
-               To purchase our token head over to our in house swap located within the DApp under the "Swap" tab, purchase 10 NeFi tokens, switch to the "Protostars" tab and create your protostar to become a Nebulan!
-            </p>
-            <div className="btn_soc">
-               <a  className="btn_ion" href="/Dapp" target={{target:"_blank"}}>
-               DApp </a>
-            </div>
+            <h1 className="h1_heading set_font"> Team</h1>
          </div>
          <div className="col-sm-12 col-md-12 col-lg-2"> 
-            <img className="image_ad" src="assets/images/sec3inner.png" />
+            <img className="image_dev" src="assets/css/nbreezay.png" />
+         </div>
+         <div className="row">
+         <p className="desc_dev">
+               nbreezay - dev
+            </p>
          </div>
       </div>
    </div>
 </section>
-<section id="map_nebula " className="map_sec ">
+<section id="map " className="info_sec ">
    <div id="roadmap" className="container-fluid ">
       <div className="row">
          <div className="col-sm-12 col-md-12 col-lg-12 text-center p-0">
@@ -113,51 +93,32 @@ function Home() {
          </div>
       </div>
       <div className="row">
-         <div className="col-sm-12 col-md-12 col-lg-12 p-0">
-            <img className="" src="assets/images/roadmap_new.png" />    
+      </div>
+   </div>
+</section>
+<section id="faq " className="info_sec ">
+   <div id="faq" className="container-fluid ">
+      <div className="row">
+         <div className="col-sm-12 col-md-12 col-lg-12 text-center p-0">
+            <h1 className="h1_heading set_font"> FAQ</h1>
          </div>
       </div>
    </div>
 </section>
 <footer>
-   <section id="footer_nebula " className="footer_sec ">
+   <section id="footer " className="footer_sec ">
       <div className="container-fluid ">
-         <div className="row">
-            <div className="col-sm-12 col-md-4 col-lg-2 text-center p-0">
-               <a className="navbar-brand navbar-footer" href="/"><img className="logo_s" src="assets/images/logo.png"/></a>                 
-            </div>
-            <div className="col-sm-12 col-md-4 col-lg-5 text-center p-0">
-            </div>
-            <div className="col-sm-12 col-md-4 col-lg-3 text-center p-0">
-               <div className="footer_links tabs-section">
-                  <ul className="navbar-nav m-auto">
-                     <li className="nav-item ">
-                        <a className="nav-link active show" href="/">Home</a>
-                     </li>
-                     <li className="nav-item">
-                        <a className="nav-link" href="#tokenmics">Tokenomics</a>
-                     </li>
-                     <li className="nav-item">
-                        <a className="nav-link" href="#buy">How To Buy</a>
-                     </li>
-                     <li className="nav-item">
-                        <a className="nav-link" href="https://docs.nebulanodes.finance">Docs</a>
-                     </li>
-                     <li className="nav-item">
-                        <a className="nav-link" href="/Dapp">Dapp</a>
-                     </li>
-                  </ul>
-               </div>
-            </div>
-         </div>
          <div className="row icons_footer">
+            <div className="col-sm-12 text-left">
+               <a className="navbar-brand navbar-footer" href="/"><img className="logo_s" src="assets/css/logo.png"/></a>                 
+            </div>
             <div className="col-sm-12 text-right">
                <span className="navbar-text" >
-               <a href="https://twitter.com/NebulaNodes" target={{target:"_blank"}}>
-               <img className="seicon" src="assets/images/icon/icon_twitter.svg"/>
+               <a href="https://twitter.com/BarbarianInc" target={{target:"_blank"}}>
+               <img className="seicon" src="assets/css/icon_twitter.svg"/>
                </a>
-               <a href="https://discord.com/invite/dk8pCznAJP" target={{target:"_blank"}}>
-               <img className="seicon" src="assets/images/icon/icon_discord.svg"/>
+               <a href="https://twitter.com/BarbarianInc" target={{target:"_blank"}}>
+               <img className="seicon" src="assets/css/icon_discord.svg"/>
                </a>
                </span>
             </div>
