08c816f81bd7835604845509a7c6f056a507d4bc
natehyatt44 <nate.hyatt@gmail.com>
2023-02-04 08:10:26 -0700

    Added LitePaper content

--- src/pages/Litepaper.js
+++ src/pages/Litepaper.js
@@ -14,12 +14,50 @@ function Litepaper() {
                <h1 className="h1_heading set_font"> Litepaper </h1>
                <br/>
                <p className="para_p bolded">MISSION STATEMENT <br/><br/> 
-                  To create a community driven ecosystem of nft collections under the umbrella of BarbarianInc, 
-                  which is a special and unique NFT company catering to the larger crypto space as a whole. 
-                  Our main goal is to establish ourselves as a brand upon which trust and value can come to be expected. 
-                  Secondary to this, but still of massive importance to the team, we hope that this attitude and outlook will catch the 
-                  attention of collectors not just within Hedera, but also with collectors from many other ecosystems outside of Hedera as well, 
-                  acting as a catalyst to help grow the user base of Hedera itself.
+               To unveil a batch of 2,000 PFP-style Community Founders Passes aimed at boosting funding for the project, 
+               and creating a buzz in the community. Our belief is that individuals will back our extensive collection of 
+               interconnected NFTs designed for the alternate reality game "The Lost Ones" and our future metaverse through 
+               the acquisition of this collection. Our team is committed to providing outstanding art and practicality, 
+               and we aim to showcase this in a big way. Our objective is to dispel the unfavorable views surrounding Community 
+               Founders Passes, and offer a thrilling and rewarding experience for our community.
+               <br/><br/>
+               What is the Value Proposition for the “Hbarbarians” Community Founders Passes?<br/><br/>
+               We are structuring this first collection to provide maximum reward and utility for early adopters of the project. 
+               With that said, here is what holding an “Hbarbarian” Community Founders Pass will provide you:
+               <br/>
+               All “Hbarbarians” Community Founders Passes will be playable as the Mortals race in the upcoming ARG (Alternate Reality Game).
+               <br/>
+               All holders will receive a free NFT airdrop from a collection curated by our very own in-house artist @alixonjviloria.
+               <br/>
+               One whitelist will be granted for each of the 5 ARG NFT collections that are to be dropped in the future. There is zero obligation to use these, they are there simply if you want or need them.
+               <br/>
+               One “Hbarbarian” Community Founders Pass holder will be deemed an instant winner of the ARG, guaranteeing that the individual who possesses it will receive his or her share of the prize pool at the end of the game.
+               <br/>
+               A permanent and ongoing commitment to providing perks and benefits to holders as the BarbarianInc ecosystem continues to evolve.
+               <br/><br/>
+               What is an Alternate Reality Game, and how will “The Lost Ones” act as an ARG?<br/><br/>
+               An ARG is a type of transmedia game that blurs the line between the game world and the real world. ARGs typically consist of a series of puzzles and 
+               challenges that players must solve in order to progress through the game. These puzzles often involve real-world elements such as websites, history,
+                and real-life events. The goal of an ARG is to create a sense of immersion, and to allow players to experience a story in a unique and interactive way. 
+                ARGs can be played by a large number of people simultaneously, and they often involve elements of mystery, adventure, and suspense. In this text-based format, 
+                the team will commence the development of the NFT-powered ARG for the community, entitled "The Lost Ones."
+               For those without an “Hbarbarian” Community Founders Pass, access to “The Lost Ones” storyline and ARG will be granted to the individual through an 
+               initial public mint of soul-bindable character NFTs. It should be noted that “Hbarbarian” Community Founders Passes do NOT need to be soul-bound.
+                If the right type of NFT from the character collection has been acquired by an individual, and they choose to have it soul-bound to a wallet in their possession, 
+                then this will provide an opportunity for them to complete the specific storyline associated with it. Determining which of these character 
+                NFTs will provide the possibility to participate will be based on word games, hints, and other scavenger hunt-style activities hosted by the team.
+                 This will be the first among many inquiries that players must answer to beat the game.
+               Ultimately, the main twist on ARG gameplay is that it will be up to the participants to hunt down and hold within the same wallet, the right
+                combination of NFTs from each collection dropped in association with “The Lost Ones”. Without the perfect lineage (combination) of NFTs held in their wallet,
+                 their text-based adventure will not be complete. Many participants will be motivated to try to discover the true, fully completed ending to the 
+                 storyline by the potential of winning large prizes denoted in $HBAR. Rewards will be funded through 15% of all profits from all 5 mints associated with
+                  “The Lost Ones” being placed in a prize pool. This will genuinely motivate and potentially even reward those who chose to commit themselves to the game.
+               <br/><br/>
+               How do "Hbarbarian" Community Founders Passes relate to our ARG?<br/><br/>
+               Each "Hbarbarian" Community Founder Pass will function as a playable character in our ARG, "The Lost Ones". It is crucial to keep in mind that not 
+               all characters that will be released in the future will be fully playable, and it will be the responsibility of the minter and/or buyer of these 
+               future characters to follow the clues to determine which are usable game pieces. However, holders of the "Hbarbarian" Community Founder Passes
+                will have no need for this initial problem-solving to determine which ones can or cannot be played with - a clear advantage for those who possess this collection.
 
                </p>
             </div>
