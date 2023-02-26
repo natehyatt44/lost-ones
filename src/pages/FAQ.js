846d68907dc79dcb4495e962eecb4d307c01601e
TrevorMann <Trevor.T.Mann@gmail.com>
2023-02-26 16:00:40 -0700

    Committing files

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -3,15 +3,22 @@ import questions from "../mappings/faq.json";
 import Banner from "../components/FaqInfo"
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
+import { Storage, API } from 'aws-amplify';
+import MailingListAdd from '../components/emailData';
 const { Slide } = require("react-awesome-reveal");
 
 
+function testSomething()
+{
+API.get('apiname', 'path')
+
+}
 
 function FAQ() {
   return (
    <> 
    <Navigation />
-   <section id="faq " className="info_sec ">
+   <section id="faq " className="background_sec ">
    <Banner>
     <Slide>
     <Banner.Header><h1 className="h1_heading set_font"> FAQ </h1></Banner.Header>
@@ -21,6 +28,8 @@ function FAQ() {
           <Banner.Text>{question.answers}</Banner.Text>
         </Banner.Entity>
       ))}
+
+     <MailingListAdd/>
       </Slide>
     </Banner>
    {}
