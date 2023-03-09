e2e61c818cbe21729f06db61ca41ea499f32ec46
natehyatt44 <nate.hyatt@gmail.com>
2023-03-09 14:47:30 -0700

    Updated Gallery Loading

--- src/components/GalleryImages.js
+++ src/components/GalleryImages.js
@@ -1,52 +1,87 @@
 import { Storage } from 'aws-amplify';
-import React, {useEffect, useState} from 'react';
-import { JackInTheBox } from 'react-awesome-reveal';
+import React, { useEffect, useState } from 'react';
+import { Fade } from 'react-awesome-reveal';
 
+// Define the number of images to display
 const numImages = 21;
 
+// Function to shuffle an array in place
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-  }
-
-function GalleryImages() {
-    const [images, setImages] = useState([])
-
-    useEffect(() => {
-      fetchImages()
-    }, [])
-
-    async function fetchImages() {
-      let imageKeys = await Storage.list('nft-founders-pass/images/', {level: 'public'})
-      let imageKeysUpdated = []
-      imageKeys.results.forEach(item => {
-        if (item.size) {
-            imageKeysUpdated.push(item)
-        }
-      });
-
-      shuffle(imageKeysUpdated)
-      imageKeysUpdated = imageKeysUpdated.slice(0,numImages)
-
-      imageKeysUpdated = await Promise.all(imageKeysUpdated.map(async k => {
-          const key = await Storage.get(k.key)  
-          return key
-      }))
-      
-      setImages(imageKeysUpdated)
-    }
-    
-    const html = images.map(image =>   
-        <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-            <JackInTheBox>
-            <div className="gallery-item">
-                <img src={image} alt="galleryimg"/>
-            </div>
-            </JackInTheBox>
-        </div>  
-        )
-    return (
-        html
-    )
+  array.sort(() => Math.random() - 0.5);
 }
 
-export default GalleryImages 
+// Component for displaying gallery images
+function GalleryImages() {
+  // Use state hooks to store the images and their load status
+  const [images, setImages] = useState([]);
+  const [loadedImages, setLoadedImages] = useState([]);
+
+  // Fetch the images on component mount
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  // Function to fetch the images from AWS S3
+  async function fetchImages() {
+    let imageKeys = await Storage.list('nft-founders-pass/images/', { level: 'public' });
+
+    // Filter out images that do not have a size property (e.g. folders)
+    let imageKeysUpdated = imageKeys.results.filter(item => item.size);
+
+    // Shuffle the array of image keys and limit it to the desired number of images
+    shuffle(imageKeysUpdated);
+    imageKeysUpdated = imageKeysUpdated.slice(0, numImages);
+
+    // Initialize the loadedImages array to all false
+    const loadedArray = new Array(imageKeysUpdated.length).fill(false);
+    setLoadedImages(loadedArray);
+
+    // Fetch the images from AWS S3 and update the state
+    imageKeysUpdated = await Promise.all(
+      imageKeysUpdated.map(async k => {
+        const key = await Storage.get(k.key);
+        return key;
+      })
+    );
+
+    setImages(imageKeysUpdated);
+  }
+
+  // Use another effect hook to update the loadedImages state as the images are loaded
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const index = loadedImages.findIndex(loaded => !loaded);
+      if (index !== -1) {
+        const newLoadedImages = [...loadedImages];
+        newLoadedImages[index] = true;
+        setLoadedImages(newLoadedImages);
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 400);
+
+    // Clean up the interval on unmount
+    return () => clearInterval(intervalId);
+  }, [loadedImages]);
+
+  // Map over the images to generate HTML for each one
+  const html = images.map((image, index) => (
+    <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 text-center" key={index}>
+      <div className="gallery-item">
+        {loadedImages[index] ? (
+          <Fade delay={500}>
+            <img src={image} alt="galleryimg" />
+          </Fade>
+        ) : (
+          <div className="loading-animation"></div>
+        )}
+      </div>
+    </div>
+  ));
+
+  // Render the HTML
+  return <div className="row">{html}</div>;
+}
+
+// Export the component as the default export
+export default GalleryImages;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,28 +1,41 @@
+import { useState, useEffect } from 'react';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 import GalleryImages from '../components/GalleryImages';
+import { Fade } from 'react-awesome-reveal';
 
 function Gallery() {
+  // State to determine if the Footer should be shown or not
+  const [showFooter, setShowFooter] = useState(false);
+
+  // Use effect to show the Footer after 3 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowFooter(true);
+    }, 3000);
     
+    // Clean up function to clear the timeout
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-      <>
+    <>
+      {/* Navigation component */}
       <Navigation />
-      <section id="Gallery " className="info_sec ">
-      <div className="container gallery-container">
-      
+
+      {/* Gallery section */}
+      <section id="Gallery" className="info_sec">
+        <div className="container gallery-container">
           <div className="row">
-            <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                <div className="row">
-                    <div className="row">
-                        <GalleryImages></GalleryImages>
-                    </div>
-                </div>
-              </div>
+            {/* GalleryImages component */}
+            <GalleryImages />
           </div>
-      </div>
+        </div>
       </section>
-      <Footer />
-      </>
+
+      {/* Footer component (only shown after 3 seconds) */}
+      {showFooter && <Fade><Footer /></Fade>}
+    </>
   );
 }
 
