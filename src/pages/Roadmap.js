dfa763da39f597b99fc8b96d0656e847f8578a42
natehyatt44 <nate.hyatt@gmail.com>
2023-05-06 12:45:45 -0600

    Raodmap Desktop

--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -13,7 +13,7 @@ function Roadmap() {
     <section id="Roadmap " className="info_sec "> 
        <div className='roadmap-container '>
             <div className='col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center p-0 roadmap-desktop-item'>
-                     <img src="assets/images/banner/barb-Roadmap-mobile.png" alt="roadmap"></img> 
+                     <img src="assets/images/banner/barb-Roadmap-desktop.png" alt="roadmap"></img> 
                </div>
                <div className='col-12 col-sm-12 col-md-12 col-lg-12 text-center p-0 roadmap-mobile-item'>
                      <img src="assets/images/banner/barb-Roadmap-mobile.png" alt="roadmap"></img> 
