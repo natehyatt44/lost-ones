886cba4e458c101a848b990d080eb978b5b2032e
natehyatt44 <nate.hyatt@gmail.com>
2023-05-11 11:04:56 -0600

    Updated discord links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -30,7 +30,7 @@ function Navigation() {
                     <a href="https://twitter.com/barbarian_inc" target={{target:"_blank"}}>
                         <img className="seicon" src="assets/images/icon/icon_twitter.svg" alt="navimg"/>
                     </a>
-                    <a href="https://discord.gg/BW2BJJKxxg" target={{target:"_blank"}}>
+                    <a href="https://discord.gg/aCucjcJtWM" target={{target:"_blank"}}>
                         <img className="seicon" src="assets/images/icon/icon_discord.svg" alt="navimg"/>
                     </a>
                     <a href="https://nftier.tech/mint/harbarians-founder-pass" target={{target:"_blank"}}>
