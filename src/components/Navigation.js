33b9a40a19f9864ac1328102d0997481d7669cff
natehyatt44 <nate.hyatt@gmail.com>
2023-07-28 20:30:13 -0600

    Home page Update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -33,9 +33,6 @@ function Navigation() {
                     <a href="https://discord.gg/aCucjcJtWM" target={{target:"_blank"}}>
                         <img className="seicon" src="assets/images/icon/icon_discord.svg" alt="navimg"/>
                     </a>
-                    <a href="https://nftier.tech/mint/hbarbarians-founder-pass-drop-2" target={{target:"_blank"}}>
-                        <img className="seicon" src="assets/images/nfttiers.logo.svg" alt="navimg"/>
-                    </a>
                     </span>
                 </div>
             </nav>
